c28695bd0bed13131125560cab7e2eb1a902c342
Tuan Anh Hoang <anhtuan.ash@gmail.com>
2022-05-18 23:49:36 +0700

    Get news by titleUrl

--- pages/api/news/[id].js
+++ pages/api/news/[id].js
@@ -1,59 +1,66 @@
-import dbConnect from '../../../lib/dbConnect'
-import cors from '../../../lib/cors'
-import News from '../../../models/News'
+import dbConnect from "../../../lib/dbConnect";
+import cors from "../../../lib/cors";
+import News from "../../../models/News";
 
 export default async function handler(req, res) {
   const {
     query: { id },
     method,
-  } = req
+  } = req;
 
-  await cors(req, res)
-
-  await dbConnect()
+  await cors(req, res);
+  await dbConnect();
 
   switch (method) {
-    case 'GET' /* Get a model by its ID */:
+    case "GET" /* Get a model by its ID */:
       try {
-        const news = await News.findById(id)
-        if (!news) {
-          return res.status(400).json({ success: false })
+        if (id.includes("-")) {
+          const news = await News.find({ titleUrl: id });
+          if (!news) {
+            return res.status(400).json({ success: false });
+          }
+          return res.status(200).json(news[0]);
         }
-        res.status(200).json(news)
-      } catch (error) {
-        res.status(400).json({ success: false })
-      }
-      break
 
-    case 'PUT' /* Edit a model by its ID */:
+        const news = await News.findById(id);
+        if (!news) {
+          return res.status(400).json({ success: false });
+        }
+        return res.status(200).json(news);
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+
+    case "PUT" /* Edit a model by its ID */:
       try {
         const news = await News.findByIdAndUpdate(id, req.body, {
           new: true,
           runValidators: true,
-        })
+        });
         if (!news) {
-          return res.status(400).json({ success: false })
+          return res.status(400).json({ success: false });
         }
-        res.status(200).json(news)
+        res.status(200).json(news);
       } catch (error) {
-        res.status(400).json({ success: false })
+        res.status(400).json({ success: false });
       }
-      break
+      break;
 
-    case 'DELETE' /* Delete a model by its ID */:
+    case "DELETE" /* Delete a model by its ID */:
       try {
-        const deletedNews = await News.deleteOne({ _id: id })
+        const deletedNews = await News.deleteOne({ _id: id });
         if (!deletedNews) {
-          return res.status(400).json({ success: false })
+          return res.status(400).json({ success: false });
         }
-        res.status(200).json({ success: true })
+        res.status(200).json({ success: true });
       } catch (error) {
-        res.status(400).json({ success: false })
+        res.status(400).json({ success: false });
       }
-      break
+      break;
 
     default:
-      res.status(400).json({ success: false })
-      break
+      res.status(400).json({ success: false });
+      break;
   }
 }
